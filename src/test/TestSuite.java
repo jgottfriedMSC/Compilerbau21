335a08d8b94ca1372b1f52259d8b0725cc43c8c6
Markus Eble <markus.eble@online.de>
2021-04-19 16:23:31 +0200

    NumberAdder Exercise

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -1,56 +1,44 @@
 package test;
 
+
 public class TestSuite extends TestSuiteIntf {
+  
+    public TestSuite(compiler.FileReaderIntf fileReader, TestCaseIntf testCase) {
+    	super(fileReader, testCase);
+    }
 
-	public TestSuite(compiler.FileReaderIntf fileReader, TestCaseIntf testCase) {
-		super(fileReader, testCase);
-	}
-
-	@Override
-	// testSequence: testCase*
-	void readAndExecuteTestSequence() throws Exception {
+	public void readAndExecuteTestSequence() throws Exception {
 		while (m_fileReader.lookAheadChar() != 0) {
 			readAndExecuteTestCase();
 		}
 	}
-
-	@Override
-	// testCase: dollarIn testContent dollarOut testContent
-	void readAndExecuteTestCase() throws Exception {
+	
+	public void readAndExecuteTestCase() throws Exception {
 		readDollarIn();
 		String input = readTestContent();
 		readDollarOut();
 		String output = readTestContent();
 		executeTestCase(input, output);
 	}
-
-	@Override
-	// testContent: [^$]*
-	String readTestContent() throws Exception {
-		// TODO Auto-generated method stub
-		String testContent = new String("");
+	
+	public String readTestContent() throws Exception {
+		String testContent = "";
 		while (m_fileReader.lookAheadChar() != '$' && m_fileReader.lookAheadChar() != 0) {
 			testContent += m_fileReader.lookAheadChar();
 			m_fileReader.advance();
 		}
 		return testContent;
 	}
-
-	@Override
-	// dollarIn: "$IN" linebreak
-	void readDollarIn() throws Exception {
-		// TODO Auto-generated method stub
+	
+	public void readDollarIn() throws Exception {
 		m_fileReader.expect('$');
 		m_fileReader.expect('I');
 		m_fileReader.expect('N');
 		m_fileReader.expect('\r');
 		m_fileReader.expect('\n');
 	}
-
-	@Override
-	// dollarOut: "$Out" linebreak
-	void readDollarOut() throws Exception {
-		// TODO Auto-generated method stub
+	
+	public void readDollarOut() throws Exception {
 		m_fileReader.expect('$');
 		m_fileReader.expect('O');
 		m_fileReader.expect('U');
@@ -58,5 +46,4 @@ public class TestSuite extends TestSuiteIntf {
 		m_fileReader.expect('\r');
 		m_fileReader.expect('\n');
 	}
-
 }
